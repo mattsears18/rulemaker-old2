8a716185f24634e8b34bd4a530b3d5404e76bc7c
Matt Sears <mattsears18@gmail.com>
2021-10-30 20:25:42 -0400

    rules query

--- src/app/components/RulesList/__tests__/index.test.tsx
+++ src/app/components/RulesList/__tests__/index.test.tsx
@@ -2,10 +2,17 @@ import * as React from 'react';
 import { render } from '@testing-library/react';
 
 import { RulesList } from '..';
+import { MockedProvider } from '@apollo/client/testing';
+
+const mocks = [];
 
 describe('<RulesList  />', () => {
   it('should match snapshot', () => {
-    const loadingIndicator = render(<RulesList />);
+    const loadingIndicator = render(
+      <MockedProvider mocks={mocks}>
+        <RulesList />
+      </MockedProvider>,
+    );
     expect(loadingIndicator.container.firstChild).toMatchSnapshot();
   });
 });
--- src/app/components/RulesList/index.tsx
+++ src/app/components/RulesList/index.tsx
@@ -5,15 +5,47 @@
  */
 import * as React from 'react';
 import styled from 'styled-components/macro';
+import { useQuery, gql } from '@apollo/client';
+
+const RULES = gql`
+  query GetRules {
+    rules_aggregate {
+      aggregate {
+        count
+      }
+    }
+    rules {
+      id
+      name
+      is_public
+      created_at
+      updated_at
+      creator_user {
+        id
+        first_name
+        last_name
+      }
+    }
+  }
+`;
 
 interface Props {}
 
 export function RulesList(props: Props) {
+  const { loading, error, data } = useQuery(RULES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) {
+    throw error;
+  }
+
   return (
     <Div>
-      <h2>Rules List</h2>
-      <div>a rule</div>
-      <div>{process.env.REACT_APP_GRAPHQL_ENDPOINT}</div>
+      <h4>Rules ({data.rules_aggregate.aggregate.count})</h4>
+      {data?.rules?.length &&
+        data.rules.map((rule: { id: string; name: string }) => {
+          return <div key={rule.id}>{rule.name}</div>;
+        })}
     </Div>
   );
 }
