582ef0e9cf6a5d088e3db16870f8329c1658cfdc
Matt Sears <mattsears18@gmail.com>
2022-03-06 11:55:16 -0500

    redwood (#57)
    Switch app to redwood

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-export const auth0Config = {
-  client_id: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID,
-  domain: process.env.NEXT_PUBLIC_AUTH0_DOMAIN,
-};
-
-export const gtmConfig = {
-  containerId: process.env.NEXT_PUBLIC_GTM_CONTAINER_ID,
-};
--- src/hooks/use-auth.ts
+++
@@ -1,4 +0,0 @@
-import { useContext } from 'react';
-import { AuthContext } from '../contexts/auth0-context';
-
-export const useAuth = () => useContext(AuthContext);
