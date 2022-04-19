9b6576bfff9807e669b4b8c1484a06a75bedc5d3
Matt Sears <mattsears18@gmail.com>
2022-04-19 11:46:26 -0400

    revert AnonLayout

--- web/src/layouts/AnonLayout/AnonLayout.stories.tsx
+++
@@ -1,7 +0,0 @@
-import AnonLayout from './AnonLayout'
-
-export const generated = () => {
-  return <AnonLayout />
-}
-
-export default { title: 'Layouts/AnonLayout' }
--- web/src/layouts/AnonLayout/AnonLayout.tsx
+++
@@ -1,9 +0,0 @@
-type AnonLayoutProps = {
-  children?: React.ReactNode
-}
-
-const AnonLayout = ({ children }: AnonLayoutProps) => {
-  return <>{children}</>
-}
-
-export default AnonLayout
