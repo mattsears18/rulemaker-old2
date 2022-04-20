d5bce744243e913ca1fee9d511681b9a6aeadcf4
Matt Sears <mattsears18@gmail.com>
2022-04-19 21:22:39 -0400

    navbar placeholder

---
+++ web/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,12 @@
+// https://chakra-templates.dev/navigation/navbar
+
+const NavBar = () => {
+  return (
+    <div>
+      <h2>{'NavBar'}</h2>
+      <p>{'Find me in ./web/src/components/NavBar/NavBar.tsx'}</p>
+    </div>
+  )
+}
+
+export default NavBar
