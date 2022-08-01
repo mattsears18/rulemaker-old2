badbc5d90438e79d6cb09c1d6f7a4254f28836c7
Matt Sears <mattsears18@gmail.com>
2022-08-01 08:44:27 -0400

    install tailwinds

---
+++ pages/rules/[id].tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Rules: NextPage = ({ id }) => {
+  return <div>Show a single rule...</div>;
+};
+
+export default Rules;
---
+++ pages/rules/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Rules: NextPage = () => {
+  return <div>List all of the rules...</div>;
+};
+
+export default Rules;
